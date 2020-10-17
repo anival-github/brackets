41da5898aa27068fbd048dac64bdfada8d2e8bfe
Valentin Anikin <anival-94@mail.ru>
2020-10-17 19:16:41 +0300

    feat: add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsConfigModified = bracketsConfig.map(function(array){
+    let stringFromArray = array[0] + array[1];
+    return stringFromArray;
+});
+console.log(bracketsConfigModified);
+
+while (bracketsConfigModified.some(elem => str.indexOf(elem) >= 0)) {
+    for (i = 0; i <= bracketsConfigModified.length - 1; i += 1) {
+        while (str.indexOf(bracketsConfigModified[i]) >= 0) {
+            str = str.replace(bracketsConfigModified[i], '');
+        }
+    }
 }
+
+if (str.length === 0) {
+    return true;
+} else {
+    return false;
+}
+}
+
